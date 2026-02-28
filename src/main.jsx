6bc63e6409cba6c59d4e0c2e90d3843349721afc
sadineniramya-champions <sadineni.ramya@gmail.com>
2026-02-28 17:59:33 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from “react”;
+import ReactDOM from “react-dom/client”;
+import StepUpCounter from “./StepUpCounter”;
+
+ReactDOM.createRoot(document.getElementById(“root”)).render(
+<React.StrictMode>
+<StepUpCounter />
+</React.StrictMode>
+);
